3e6f289a8d7841286a555b0ac2933c6524b0c24d
Gullitan <1994slavaslava@gmail.com>
2017-12-29 04:43:14 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -3,5 +3,45 @@ jQuery.fn.exists = function () {
 };
 
 jQuery(document).ready(function ($) {
+    $('.owl-carousel1').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
 
+    $('.owl-carousel2').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 1
+            },
+            992: {
+                items: 2
+            },
+            1200: {
+                items: 2
+            }
+        }
+    });
+    $(".program-info").mCustomScrollbar({
+        theme: "dark",
+        scrollButtons: {scrollType: "stepped"},
+        live: "on"
+    });
 });
